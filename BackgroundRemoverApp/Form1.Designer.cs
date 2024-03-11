5d7f0596a37558be1df0082294d6e9b5c816e35a
Patbodin Intajak <patbodin30443@hotmail.com>
2024-03-11 20:57:40 +0700

    Complete a draft version of the app

--- BackgroundRemoverApp/Form1.Designer.cs
+++ BackgroundRemoverApp/Form1.Designer.cs
@@ -29,13 +29,78 @@ namespace BackgroundRemoverApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.btnRemoveBackground = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.txtColorCode = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(41, 35);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(328, 283);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(446, 56);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(144, 38);
+            this.btnLoadImage.TabIndex = 1;
+            this.btnLoadImage.Text = "LoadImage";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // btnRemoveBackground
+            // 
+            this.btnRemoveBackground.Location = new System.Drawing.Point(446, 125);
+            this.btnRemoveBackground.Name = "btnRemoveBackground";
+            this.btnRemoveBackground.Size = new System.Drawing.Size(144, 38);
+            this.btnRemoveBackground.TabIndex = 2;
+            this.btnRemoveBackground.Text = "RemoveBackground";
+            this.btnRemoveBackground.UseVisualStyleBackColor = true;
+            this.btnRemoveBackground.Click += new System.EventHandler(this.btnRemoveBackground_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // txtColorCode
+            // 
+            this.txtColorCode.Location = new System.Drawing.Point(446, 197);
+            this.txtColorCode.MaxLength = 0;
+            this.txtColorCode.Name = "txtColorCode";
+            this.txtColorCode.Size = new System.Drawing.Size(144, 22);
+            this.txtColorCode.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.txtColorCode);
+            this.Controls.Add(this.btnRemoveBackground);
+            this.Controls.Add(this.btnLoadImage);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "RemoveBackgroudApp";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.Button btnRemoveBackground;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox txtColorCode;
     }
 }
 
--- BackgroundRemoverApp/Form1.cs
+++ BackgroundRemoverApp/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,64 @@ namespace BackgroundRemoverApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Image = new Bitmap(openFileDialog.FileName);
+            }
+            
+        }
+
+        private void btnRemoveBackground_Click(object sender, EventArgs e)
+        {
+            if (pictureBox.Image == null)
+            {
+                MessageBox.Show("Please load an image first.");
+                return;
+            }
+
+            string colorCode = txtColorCode.Text.TrimStart('#');
+            if (!int.TryParse(colorCode, System.Globalization.NumberStyles.HexNumber, null, out int argb))
+            {
+                MessageBox.Show("Please enter a valid hex color code.");
+                return;
+            }
+
+            Color removeColor = Color.FromArgb(argb);
+            Bitmap updatedImage = RemoveBackgroundColor((Bitmap)pictureBox.Image, removeColor);
+            pictureBox.Image = updatedImage;
+        }
+
+        private Bitmap RemoveBackgroundColor(Bitmap image, Color bgColor)
+        {
+            Bitmap newImage = new Bitmap(image.Width, image.Height, PixelFormat.Format32bppArgb);
+            Graphics gfx = Graphics.FromImage(newImage);
+
+            gfx.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceOver;
+
+            // Draw the image on the new bitmap, which has a transparent background
+            gfx.DrawImage(image, 0, 0);
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    if (image.GetPixel(x, y) == bgColor)
+                    {
+                        // Set the pixel to transparent
+                        newImage.SetPixel(x, y, Color.Transparent);
+                    }
+                }
+            }
+
+            gfx.Dispose();
+            return newImage;
         }
     }
 }
